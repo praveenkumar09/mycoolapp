dbc78302eb55f49a02a5306efefccbdf7aa23e19
praveenkumar09 <tpkprav@gmail.com>
2024-12-14 11:40:44 +0800

    Initial commit for students

---
+++ src/main/java/com/praveen/spring_boot/demo/mycoolapp/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.praveen.spring_boot.demo.mycoolapp.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
