08c2edc9c3824cbca96fb062ff59c1bc05be3fa5
praveenkumar09 <tpkprav@gmail.com>
2024-12-18 14:03:35 +0800

    Added demo for reading properties for students

--- src/main/java/com/praveen/spring_boot/demo/mycoolapp/controller/HelloWorldController.java
+++ src/main/java/com/praveen/spring_boot/demo/mycoolapp/controller/HelloWorldController.java
@@ -1,14 +1,32 @@
 package com.praveen.spring_boot.demo.mycoolapp.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${coach.name}")
+    private String name;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
     @GetMapping("/")
     public String helloWorld() {
         return "Hello World";
+    }
+
+    @GetMapping("/coachName")
+    public String coachName() {
+        return name;
+    }
+
+    @GetMapping("/teamName")
+    public String teamName() {
+        return teamName;
     }
 }
